97861b1fd404e922d8ce3d0c412c2a1c2fcc1e8e
MahumManzar <132516586+MahumManzar@users.noreply.github.com>
2024-04-16 18:14:52 -0400

    Add files via upload

---
+++ Portfolio1.js
@@ -0,0 +1,9 @@
+function redirectToLinkedIn() {
+    window.location.href = "https://www.linkedin.com/in/mahum-manzar";
+  }
+
+  function redirectToProjects() {
+    window.location.href = "projects.html"; 
+  }
+
+  
